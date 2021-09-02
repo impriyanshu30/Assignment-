48a64eb43f0c63f7057a473c4d0d693403fd4f10
PRIYANSHU GUPTA <priyanshugupta627@gmail.com>
2021-09-02 18:46:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  fn : any;
+  sn: any;
+  ans : any;
+
+  add()
+  {
+    this.ans=parseInt(this.fn)+parseInt(this.sn);
+  }
+  sub()
+  {
+    this.ans=parseInt(this.fn)-parseInt(this.sn);
+  }
+  mul()
+  {
+    this.ans=parseInt(this.fn)*parseInt(this.sn);
+  }
   title = 'newproject';
 }
